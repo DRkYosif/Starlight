d5fd6d1be786beca722a47e26c40de8b5db19537
Pok245 <113675512+Pok27@users.noreply.github.com>
2025-08-01 01:45:46 +0300

    growth correction

--- Content.Server/Botany/Systems/PlantHolderSystem.cs
+++ Content.Server/Botany/Systems/PlantHolderSystem.cs
@@ -178,7 +178,7 @@ public sealed partial class PlantHolderSystem : EntitySystem
                     ("seedName", name),
                     ("seedNoun", noun)), args.User, PopupType.Medium);
 
-                component.Seed = seed;
+                component.Seed = seed.Clone();
                 component.Dead = false;
                 component.Age = 1;
                 if (seeds.HealthOverride != null)
