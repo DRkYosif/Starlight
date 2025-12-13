c1b426792df97fe38e50b7f75c09efc1b3e9959e
Pok245 <113675512+Pok27@users.noreply.github.com>
2025-12-13 15:19:44 +0200

    oops

--- Content.Server/Botany/Systems/PlantHolderSystem.cs
+++ Content.Server/Botany/Systems/PlantHolderSystem.cs
@@ -456,7 +456,6 @@ public sealed partial class PlantHolderSystem : EntitySystem
 
         CheckHealth(uid, component);
 
-
         if (component.UpdateSpriteAfterUpdate)
             UpdateSprite(uid, component);
     }
